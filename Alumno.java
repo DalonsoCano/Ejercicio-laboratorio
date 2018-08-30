5ad90daa951c67d528b7b424850b3f488e2c4399
Diego Alonso Sanchez Cano <dalonso_cano@hotmail.com>
2018-08-30 10:01:23 -0500

    Intercambio inicial de proyecto

---
+++ Alumno.java
@@ -0,0 +1,17 @@
+public class Alumno
+{
+    private String nombre;
+    private String clave;
+    
+    public Alumno(String nombre, String clave)
+    {
+        this.nombre=nombre;
+        this.clave=clave;
+    }
+    
+    public String toString()
+    {
+        
+    }
+    
+}
---
+++ Laboratorio.java
@@ -0,0 +1,12 @@
+import java.util.ArrayList;
+
+public class Laboratorio
+{
+    private ArrayList <alumno> alumno;
+    private 
+    
+    public void darDeAlta()
+    {
+        
+    }    
+}
